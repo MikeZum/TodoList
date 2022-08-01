bf1b868112fd1f92066c5e3f44c9c762a433ff32
MikeZum <100753439+MikeZum@users.noreply.github.com>
2022-08-01 11:46:51 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const toDoData = localStorage.getItem('toDo') ? JSON.parse(localStorage.getItem('toDo')) : [];
+
+const render = function () {
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+    localStorage.setItem('toDo', JSON.stringify(toDoData));
+
+    toDoData.forEach(function (item, index) {
+        const li = document.createElement('li');
+
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+        });
+
+        li.querySelector('.todo-remove').addEventListener('click', function () {
+            toDoData.splice(index, 1);
+            render();
+        });
+    });
+};
+
+render();
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (headerInput.value.trim() == '') {
+        alert('Нельзя добавить пустоту');
+        headerInput.value = '';
+    } else {
+        const newToDo = {
+            text: headerInput.value,
+            completed: false
+        };
+
+        toDoData.push(newToDo);
+        headerInput.value = '';
+
+        render();
+    }
+});
